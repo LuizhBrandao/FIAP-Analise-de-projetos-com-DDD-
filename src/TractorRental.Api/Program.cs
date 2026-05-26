cacf078ec528844895ac912175da9531670202b9
LuizhBrandao <luizh.brandaoo@gmail.com>
2026-05-26 20:39:53 -0300

    feat: gera migrations iniciais e conecta API ao Entity Framework

--- src/TractorRental.Api/Program.cs
+++ src/TractorRental.Api/Program.cs
@@ -1,12 +1,15 @@
+using TractorRental.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+// Adiciona o gerador de documentação da API
 builder.Services.AddOpenApi();
+
+// Aqui chamamos o método que criamos na Infraestrutura (Agora ele vai encontrar!)
+builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
@@ -14,28 +17,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+// Endpoint raiz para validar que a API está de pé
+app.MapGet("/", () => "Tractor Rental API Rodando com DDD e Clean Architecture!");
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
